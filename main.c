ad3a0c5408195b9d9228effc1ebce457683f4ae1
yundowon <70631088+yundowon@users.noreply.github.com>
2020-11-12 15:45:44 +0900

    03

--- main.c
+++ main.c
@@ -5,14 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i = 10;
-	int *ptr = &i;
+	int i = 100;
+	int *p = &i;
+	int **q = &p;
 	
-	printf("i = %d, pi: %p\n", i, ptr);
-	(*ptr)++;
-	printf("i = %d, pi: %p\n", i, ptr);
-	*ptr++;
-	printf("i = %d, pi: %p\n", i, ptr);
+	*p = 200;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
+	
+	**q = 300;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
 	
 	return 0;
 }
